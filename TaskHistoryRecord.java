5b801ef1fdaa5fcb5f156885c838c43273ced94f
hedinant <hedinant@gmail.com>
2020-04-22 13:56:14 +0300

    Create TaskHistoryRecord.java

---
+++ TaskHistoryRecord.java
@@ -0,0 +1,30 @@
+@Entity
+@Table(name = "taskhistory")
+public class TaskHistoryRecord {
+    @Id
+    private long id;
+    private String record;
+
+    public TaskHistoryRecord() {
+    }
+    public TaskHistoryRecord(String record) {
+        this.record = record;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRecord() {
+        return record;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+}
